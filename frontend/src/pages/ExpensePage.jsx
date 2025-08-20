a4d15018de1ab491a162dd5ddfa24306a381f95d
caleb-jung123 <caleb10131013@gmail.com>
2025-08-20 03:06:00 -0400

    refactor navbar #4

--- frontend/src/pages/ExpensePage.jsx
+++ frontend/src/pages/ExpensePage.jsx
@@ -3,9 +3,9 @@ import ExpenseList from "../components/ExpenseList"
 
 function ExpensePage() {
     return (
-        <div className="flex w-full h-screen overflow-hidden">
+        <div className="flex w-full h-screen overflow-hidden bg-gray-50">
             <NavBar />
-            <div className="flex-1 overflow-auto">
+            <div className="flex-1 overflow-auto bg-gray-50">
                 <ExpenseList />
             </div>
         </div>
